214bae1f33ac5d98aeebcd53f2c6458b9db41409
Ryan <stander.ryan@gmail.com>
2025-05-20 00:51:16 +0200

    Main Menu

---
+++ Assets/Scripts/UI/MainMenu/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class PlayButton : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            // Load the game scene here
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/QuitButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI.MainMenu
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+            // Quit the application
+            Application.Quit();
+        }
+    }
+}
